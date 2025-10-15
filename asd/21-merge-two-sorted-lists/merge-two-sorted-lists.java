607da340eaea6885824192520dfb8c8fe6155208
TharinduAma <tamarasinghage@csumb.edu>
2025-10-14 21:40:36 -0700

    Update merge-two-sorted-lists.java

--- asd/21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ asd/21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -11,9 +11,7 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode temp = new ListNode(-1);
-
         ListNode current = temp;
-
         while (list1 != null && list2 != null) {
             if (list1.val <= list2.val) {
                 current.next = list1;
@@ -24,13 +22,11 @@ class Solution {
             }
             current = current.next;
         }
-
         if (list1 != null) {
             current.next = list1;
         } else {
             current.next = list2;
         }
-
         return temp.next;
     }
 }
